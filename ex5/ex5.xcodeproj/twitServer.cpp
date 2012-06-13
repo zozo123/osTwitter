a71073b3d750efdb841ddf35e5ffbf949e1f79f5
zozo123 <eliaz123@gmail.com>
2012-06-13 13:48:55 +0300

    tutorial added

--- ex5/ex5.xcodeproj/twitServer.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  twitServer.cpp
-//  
-//
-//  Created by yossi eliaz on 6/13/12.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
-//
-
-#include <iostream>
---
+++ toturial/toturial/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  toturial
+//
+//  Created by yossi eliaz on 6/13/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
